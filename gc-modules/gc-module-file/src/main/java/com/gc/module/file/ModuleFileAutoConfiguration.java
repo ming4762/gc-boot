8bbe56e972d93e00fd797ab3a8c575bbfb73d381
zhongming4762 <zhongming4762@hotmail.com>
2021-03-08 18:00:52 +0800

    完善文件模块

--- gc-modules/gc-module-file/src/main/java/com/gc/module/file/ModuleFileAutoConfiguration.java
+++ gc-modules/gc-module-file/src/main/java/com/gc/module/file/ModuleFileAutoConfiguration.java
@@ -5,7 +5,6 @@ import com.gc.module.file.service.SysFileService;
 import com.gc.module.file.service.impl.DefaultFileServiceImpl;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -21,13 +20,12 @@ public class ModuleFileAutoConfiguration {
 
     /**
      * 创建文件服务实体类
-     * @param applicationContext spring上下文
      * @param properties 参数
      * @return 文件服务实体类
      */
     @Bean
     @ConditionalOnMissingBean(SysFileService.class)
-    public SysFileService sysFileService(ApplicationContext applicationContext, SmartFileProperties properties) {
-        return new DefaultFileServiceImpl(applicationContext, properties);
+    public SysFileService sysFileService(SmartFileProperties properties) {
+        return new DefaultFileServiceImpl(properties);
     }
 }
